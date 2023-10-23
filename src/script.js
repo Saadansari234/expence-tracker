25ae67f08babe6b872f960ba1813c9f69a6b6c44
Saadansari234 <saadansari3212@gmail.com>
2023-10-23 23:22:07 +0530

    added functionality on deposit button

--- src/script.js
+++ src/script.js
@@ -17,8 +17,9 @@ previousDate.addEventListener("click", () => {
 })
 
 // making an array to store all the expences
+var AllExpenses = []
 
-let AllExpences=[]
+var totalIncomeAmount = Number(document.getElementById("total-income-amount").textContent);
 
 
 // calling expence form
@@ -59,38 +60,88 @@ faPlus.addEventListener("click", () => {
       expencelists.appendChild(h4)
       expencelists.appendChild(p)
       expencelists.appendChild(div)
-      
-      
+
+
       let catagoryElem = document.createTextNode(`Catagory: ${expenceType}`);
       let AmountElem = document.createTextNode(`Rs.${expenceAmt}`)
       let cancelElem = document.createTextNode(`Delete`)
-      
-      
-      
+
+
+
       if (expenceForm.style.display == "flex") {
          expenceForm.style.display = "";
          h4.appendChild(catagoryElem)
-         p.appendChild(AmountElem)   
+         p.appendChild(AmountElem)
          div.appendChild(cancelElem)
          expenceList.appendChild(expencelists)
-         AllExpences.push(expenceAmt)
+         AllExpenses.push(expenceAmt)
+
+         // Calculate the total expense amount after adding expenses
+         let totalExpAmount = document.getElementById("total-exp-amount");
+         let expensesCount = AllExpenses.reduce(getSum, 0);
+         totalExpAmount.innerText = `Rs.${expensesCount}`
+
+
+         // Calculate the total amount after subtracting expenses from income
+         let totalAmount = document.getElementById("total-amount");
+
+         let total = totalIncomeAmount - expensesCount;
+         totalAmount.innerText = `Rs.${total}`;
+
+
       }
-
-
-      
    })
-   
-   
-   
+
 })
 
 
-// counting total expences made for that day
-
-let total_expences_count=0;
-
-for (let i = 0; i < AllExpences.length; i++) {
-   total_expences_count += AllExpences[i]
-   
-   console.log(total_expences_count)
+function getSum(total, num) {
+   return total + Math.round(num);
 }
+
+
+
+
+// adding deposit button even to add deposit in income
+let Deposit= document.getElementById("deposit")
+
+Deposit.addEventListener("click", ()=>{
+
+   let incomeForm= document.getElementById("income-form")
+   let incomeCancelBtn= document.getElementById("income-cancel-btn")
+   let incomeBtn = document.getElementById("income-btn");
+   if (incomeForm.style.display=="") {
+      incomeForm.style.display="flex"
+   }
+
+   incomeCancelBtn.addEventListener("click", ()=>{
+
+      if (incomeForm.style.display=="flex") {
+         incomeForm.style.display="";
+      }
+   })
+
+   incomeBtn.addEventListener("click", ()=>{
+
+      var totalIncome=0;
+
+      if (incomeForm.style.display=="flex") {
+         incomeForm.style.display="";
+let incomeAmt = parseInt(document.getElementById("income-amt").value);
+    
+    // Get the current total income from the "total-income-amount" element
+   //  let totalIncomeAmount = document.getElementById("total-income-amount");
+   let currentTotalAmount = parseInt(totalIncomeAmount.innerText);
+    
+    // Calculate the new total income
+    let newTotalIncome = currentTotalAmount + incomeAmt;
+    
+    // Update the "total-income-amount" element with the new total income
+    totalIncomeAmount.innerText = `Rs.${newTotalIncome}`;
+      }
+   })
+
+
+})
+
+
